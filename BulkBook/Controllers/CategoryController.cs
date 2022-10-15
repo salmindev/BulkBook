2748c4b2085df46737b53031f2c4706eacc80c29
salmindev <salmin20shafii@gmail.com>
2022-10-15 23:10:18 +0300

    Add project files.

---
+++ BulkBook/Controllers/CategoryController.cs
@@ -0,0 +1,126 @@
+﻿using BulkBook.data;
+using BulkBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> obj = _db.Categories;
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categorySingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category objs)
+        {
+            if (objs.Name == objs.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the DisplayOrder can't be same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(objs);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(objs);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category objs)
+        {
+            if (objs.Name == objs.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the DisplayOrder can't be same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(objs);
+                _db.SaveChanges();
+                TempData["success"] = "Category Update successfully";
+                return RedirectToAction("Index");
+            }
+            return View(objs);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categorySingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeletePost(int? id)
+
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
+
---
+++ BulkBook/data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkBook.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
